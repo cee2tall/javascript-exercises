79dc73e22739faa849c42ecfebb852afa5284510
cee2tall <182031182+cee2tall@users.noreply.github.com>
2025-08-25 20:30:40 -0600

    Finish javascript exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  return nums.reduce((sum, curr) => sum + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  return nums.reduce((product, curr) => product * curr);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let curr = 1;
+  for (a; a > 0; a--) {
+    curr *= a;
+  }
+  return curr;
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
